0c0b6f1d451e027e3e3ff0c0b48ca53cd9ab9829
oqo0 <oq.roman@gmail.com>
2024-07-09 17:25:38 +0300

    Add error callback signature

---
+++ FinPay.API/Signatures/Impl/ErrorCallbackSignature.cs
@@ -0,0 +1,15 @@
+﻿using FinPay.API.Utils;
+
+namespace FinPay.API.Signatures.Impl;
+
+public class ErrorCallbackSignature(
+    string merchantKey2,
+    int paymentId,
+    string paymentGenerationSignatureHash)
+    : ISignature
+{
+    public string GetParameters() => SignatureGenerator.Get(
+        merchantKey2,
+        paymentId.ToString(),
+        paymentGenerationSignatureHash);
+}
