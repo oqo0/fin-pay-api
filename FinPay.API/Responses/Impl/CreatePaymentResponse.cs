aeb1170f8e5dabde0ef7d93c51aaa76f1a750f74
oqo0 <oq.roman@gmail.com>
2024-07-09 18:15:11 +0300

    Add create payment response

---
+++ FinPay.API/Responses/Impl/CreatePaymentResponse.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinPay.API.Responses.Impl;
+
+public class CreatePaymentResponse
+{
+    [JsonPropertyName("url")]
+    public required string Url { get; set; }
+
+    #pragma warning disable CS9042
+    // Members attributed with 'ObsoleteAttribute' should not be required unless the containing type is obsolete or all constructors are obsolete.
+    [JsonPropertyName("payment_widget"), Obsolete]
+    public required string PaymentWidget { get; set; }
+    
+    [JsonPropertyName("id")]
+    public required int Id { get; set; }
+    
+    [JsonPropertyName("pay_id")]
+    public required string PayId { get; set; }
+}
