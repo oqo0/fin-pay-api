785586afd03d8a86c05e9bce0d49e798c8b0222b
oqo0 <oq.roman@gmail.com>
2024-07-09 18:37:05 +0300

    Add payment was paid callback

---
+++ FinPay.API/Callbacks/ICallback.cs
@@ -0,0 +1,9 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinPay.API.Callbacks;
+
+public interface ICallback
+{
+    [JsonPropertyName("error_code")]
+    public string? ErrorCode { get; set; }
+}
---
+++ FinPay.API/Callbacks/Impl/PaymentPaidCallback.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinPay.API.Callbacks.Impl;
+
+public class PaymentPaidCallback : ICallback
+{
+    [JsonPropertyName("id")]
+    public required int Id { get; set; }
+    
+    [JsonPropertyName("pay_id")]
+    public required string PayId { get; set; }
+    
+    [JsonPropertyName("invoice_id")]
+    public required int InvoiceId { get; set; }
+    
+    [JsonPropertyName("shop_id")]
+    public required int ShopId { get; set; }
+    
+    [JsonPropertyName("description")]
+    public required string Description { get; set; }
+    
+    [JsonPropertyName("amount")]
+    public required int Amount { get; set; }
+    
+    [JsonPropertyName("signature")]
+    public required string Signature { get; set; }
+    
+    public string? ErrorCode { get; set; }
+}
