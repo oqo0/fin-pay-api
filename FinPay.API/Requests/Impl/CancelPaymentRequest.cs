755244c04be63884d841118a884e6d6ee1f04b88
oqo0 <oq.roman@gmail.com>
2024-07-09 18:04:00 +0300

    Add cancel payment request

---
+++ FinPay.API/Requests/Impl/CancelPaymentRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinPay.API.Requests.Impl;
+
+public class CancelPaymentRequest
+{
+    [JsonPropertyName("id")]
+    public required int PaymentId { get; set; }
+
+    [JsonPropertyName("signature")]
+    public required string Signature { get; set; }
+}
