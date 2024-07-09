d5ea7a7c823b1039aec639cbc045011d13a183dc
oqo0 <oq.roman@gmail.com>
2024-07-09 17:57:08 +0300

    Add create payment request

---
+++ FinPay.API/Requests/Impl/CreatePaymentRequest.cs
@@ -0,0 +1,30 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinPay.API.Requests.Impl;
+
+public class CreatePaymentRequest : IRequest
+{
+    [JsonPropertyName("shop_id")]
+    public required int ShopId { get; set; }
+    
+    [JsonPropertyName("invoice_id")]
+    public required int InvoiceId { get; set; }
+    
+    [JsonPropertyName("description")]
+    public required string Description { get; set; }
+    
+    [JsonPropertyName("amount")]
+    public required int Amount { get; set; }
+    
+    [JsonPropertyName("method")]
+    public required string PaymentMethod { get; set; }
+    
+    [JsonPropertyName("country")]
+    public required string CountryCode { get; set; }
+    
+    [JsonPropertyName("currency")]
+    public required string Currency { get; set; }
+
+    [JsonPropertyName("signature")]
+    public required string Signature { get; set; }
+}
