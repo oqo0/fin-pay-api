146a30c5bafd47836dbc01357a937b0721dff7af
oqo0 <oq.roman@gmail.com>
2024-07-09 17:17:00 +0300

    Add success callback signature

---
+++ FinPay.API/Signatures/Impl/SuccessCallbackSignature.cs
@@ -0,0 +1,10 @@
+﻿namespace FinPay.API.Signatures.Impl;
+
+public class SuccessCallbackSignature(
+    string merchantKey2,
+    int paymentId,
+    string paymentGenerationSignatureHash)
+    : ISignature
+{
+    public string GetParameters() => $"{merchantKey2}:{paymentId}:{paymentGenerationSignatureHash}";
+}
