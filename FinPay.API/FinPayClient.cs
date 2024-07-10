b5d47b786ecdf02b8180ba6fc41f2e62767a05fd
oqo0 <oq.roman@gmail.com>
2024-07-10 08:58:23 +0300

    Implement create payment async method

--- FinPay.API/FinPayClient.cs
+++ FinPay.API/FinPayClient.cs
@@ -1,6 +1,30 @@
-﻿namespace FinPay.API;
+﻿using FinPay.API.Requests.Impl;
+using FinPay.API.Responses.Impl;
+using FinPay.API.Signatures.Impl;
 
-public class FinPayClient(int shopId, string key1, string key2)
+namespace FinPay.API;
+
+/// <summary>
+/// Клиент для работы с FinPay
+/// </summary>
+/// <param name="shopId">ID мерчанта</param>
+/// <param name="key1">Ключ мерчанта №1</param>
+/// <param name="key2">Ключ мерчанта №2</param>
+public class FinPayApiClient(int shopId, string key1, string key2) : ApiClient(ApiUrl)
 {
-    private const string ApiUrl = "api.finpay.llc/payments";
+    private const string ApiUrl = "https://api.finpay.llc/payments";
+
+    public async Task<CreatePaymentResponse> CreatePaymentAsync(CreatePaymentRequest request)
+    {
+        return await SendRequestAsync<CreatePaymentResponse>(
+            request, 
+            "", 
+            new PaymentCreationSignature(
+                shopId,
+                request.InvoiceId,
+                request.Amount,
+                request.PaymentMethod,
+                key1),
+            HttpMethod.Post);
+    }
 }
