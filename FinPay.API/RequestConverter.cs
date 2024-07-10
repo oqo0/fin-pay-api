8c96361bad7b6fe2aa1113b100e1a232c5208a57
oqo0 <oq.roman@gmail.com>
2024-07-10 09:07:47 +0300

    Change exception type

--- FinPay.API/RequestConverter.cs
+++ FinPay.API/RequestConverter.cs
@@ -8,7 +8,7 @@ public class RequestConverter : JsonConverter<IRequest>
 {
     public override IRequest Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
     {
-        throw new NotImplementedException("Deserialization is not implemented.");
+        throw new InvalidOperationException("IRequest deserialization not supported");
     }
 
     public override void Write(Utf8JsonWriter writer, IRequest value, JsonSerializerOptions options)
