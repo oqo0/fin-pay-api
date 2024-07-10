ff882f8750718dfbb4e86dd742a97b3bcc4b9ad7
oqo0 <oq.roman@gmail.com>
2024-07-10 08:57:44 +0300

    Add `IRequest` JSON converter

---
+++ FinPay.API/RequestConverter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using FinPay.API.Requests;
+
+namespace FinPay.API;
+
+public class RequestConverter : JsonConverter<IRequest>
+{
+    public override IRequest Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        throw new NotImplementedException("Deserialization is not implemented.");
+    }
+
+    public override void Write(Utf8JsonWriter writer, IRequest value, JsonSerializerOptions options)
+    {
+        var type = value.GetType();
+        JsonSerializer.Serialize(writer, value, type, options);
+    }
+}
